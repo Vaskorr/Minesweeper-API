8aa05dabdb427a9f3577f6dffbcac1cc614fb53c
Vaskorr <basil.suntsov@yandex.ru>
2024-03-22 22:56:55 +0300

    initial commit

---
+++ src/main/java/com/vaskorr/minesweeper_api/MinesweeperApiApplication.java
@@ -0,0 +1,13 @@
+package com.vaskorr.minesweeper_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperApiApplication.class, args);
+	}
+
+}
